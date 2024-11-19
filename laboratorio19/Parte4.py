3c849b5a95f9e814902b96a1b487f1919226329f
jose2024pro <joseangellopez@unicesar.edu.co>
2024-11-18 21:02:05 -0500

    actualizacion2

--- laboratorio19/Parte4.py
+++ laboratorio19/Parte4.py
@@ -1,15 +1,17 @@
 # PARTE 4: Combinación de Estructuras
 estudiantes = { "Juan": 5.0,"Ana": 4.2,"Luis": 4.5}
-
-nombre = input("Ingrese el nombre del estudiante: ")
-calificacion = float(input("Ingrese la calificación del estudiante: "))
-
-if nombre in estudiantes:
-    estudiantes[nombre] = calificacion 
-    print(f"La calificación de {nombre} ha sido actualizada.")
-else:
-    estudiantes[nombre] = calificacion  
-    print(f"{nombre} ha sido agregado con una calificación de {calificacion}.")
+n=int(input("Cuantos estudiantes vas a procesar: "))
+x=1
+while x<=n:
+    nombre = input("Ingrese el nombre del estudiante: ")
+    calificacion = float(input("Ingrese la calificación del estudiante: "))
+    if nombre in estudiantes:
+        estudiantes[nombre] = calificacion 
+        print(f"La calificación de {nombre} ha sido actualizada.")
+    else:
+        estudiantes[nombre] = calificacion  
+        print(f"{nombre} ha sido agregado con una calificación de {calificacion}.")
+    x+=1
 
 print("\nListado de estudiantes y calificaciones:")
 for estudiante, cal in estudiantes.items():
